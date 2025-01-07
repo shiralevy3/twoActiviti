914b6417508ffe0149dcde2102b62351bf5255e1
shiralevy3 <shiralevy333@gmail.com>
2025-01-08 00:36:41 +0200

    com

--- app/src/main/java/com/example/twoactivi/SecActivity2.java
+++ app/src/main/java/com/example/twoactivi/SecActivity2.java
@@ -1,24 +1,32 @@
 package com.example.twoactivi;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SecActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sec2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.buttonBack);
+
+        String userInput = getIntent().getStringExtra("userInput");
+        textView.setText(userInput);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              finish();
+            }
         });
     }
 }
+
+
