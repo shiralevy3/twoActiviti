a7bdffeb04fc8e0f3ef75451e80582bd60230832
shiralevy3 <shiralevy333@gmail.com>
2025-01-08 13:24:00 +0200

    com

--- app/src/main/java/com/example/twoactivi/MainActivity.java
+++ app/src/main/java/com/example/twoactivi/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
+
         EditText editText = findViewById(R.id.editText);
         Button button = findViewById(R.id.button);
 
